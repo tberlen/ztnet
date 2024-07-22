e9959751f410afafb774835c2f8830882e6eb69c
tberlen <tberlen@gmail.com>
2024-07-22 14:50:50 -0400

    Update init-db.sh

--- init-db.sh
+++ init-db.sh
@@ -45,7 +45,7 @@ function apply_path {
 
 # apply_path
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" -p "$POSTGRES_PORT" -d "$POSTGRES_DB" -c '\q'; do
   echo "Postgres is unavailable - sleeping"
   sleep 1
 done
